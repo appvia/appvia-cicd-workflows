7228cee91f412e5ee3087230dc20e407ed10f652
Rohith Jayawardene <gambol99@gmail.com>
2024-04-30 16:25:56 +0100

    chore: updating the max length to 200, as a number of dependabot pr get hit

--- config/commitlint.config.js
+++ config/commitlint.config.js
@@ -2,10 +2,10 @@ module.exports = {
   parserPreset: "conventional-changelog-conventionalcommits",
   rules: {
     "body-leading-blank": [1, "always"],
-    "body-max-line-length": [2, "always", 100],
+    "body-max-line-length": [2, "always", 200],
     "footer-leading-blank": [1, "always"],
-    "footer-max-line-length": [2, "always", 100],
-    "header-max-length": [2, "always", 100],
+    "footer-max-line-length": [2, "always", 200],
+    "header-max-length": [2, "always", 200],
     "subject-case": [
       2,
       "never",
